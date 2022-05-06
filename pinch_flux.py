75d1460f4c32f1eee139aa7f1e1c7d4303216dfe
jbasfernandez <J.BasFernandez@student.tudelft.nl>
2022-05-06 11:05:22 +0200

    Add flux at the pinch

---
+++ pinch_flux.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+n_particles = 45000 # evenly distributed across the orbit
+r_earth = 6378
+altitude = 800
+mu = 369800.44
+period = 2*np.pi*np.sqrt((r_earth+altitude)**3/mu)
+i_scatter = 1*np.pi/180
+h_scatter = 50
+v_scatter = np.sin(i_scatter)*(r_earth+altitude)
+area = h_scatter*v_scatter
+frequency = n_particles/period
+print('Frequency: ' + str(frequency) + ' debris fragments per second')
+flux = frequency/(area*10**6)*10
+print('The flux at the pinch points would be of: ' + str(flux) + ' debris fragments per square meter')
+
