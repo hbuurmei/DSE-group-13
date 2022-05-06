bda64d285ed58d6eda043e83fcc86c6bac6f1c4d
jbasfernandez <J.BasFernandez@student.tudelft.nl>
2022-05-06 13:37:21 +0200

    Add initial calculations for re-entry deltaV

--- DeltaVEoL.py
+++ DeltaVEoL.py
@@ -1,21 +1,56 @@
-'''Optimisation of the Delta V required for end of life'''
+''' Optimisation of the Delta V required for end of life '''
 
 
 import numpy as np
-
+from matplotlib import pyplot as plt
 
 ##### CONSTANTS #####
-initial_orbital_altitude = [350, 400, 450] #Examples [km]
+dry_masses = np.arange(100, 500)
+initial_orbital_altitude = np.array([350, 800, 1200]) #Examples [km]
 altitude_for_reentry_dueToDrag = 100 #Example [km]
 mass_Earth = 5.972 * (10 ** 24) #[kg]
 Grav_constant = 0.0000000000667 #[m^3/kg/s^2]
 radius_Earth = 6371 #[km]
-Prop_density = [1021, 1450, 1036] #Hydrazine, Hydrogen peroxide, Propyl nitrate
+Prop_density = np.array([1021, 1450, 1036]) #Hydrazine, Hydrogen peroxide, Propyl nitrate
 Tank_density = 4430 #Monopropellant tank made of Titanium 6A1-4V
-
+new_dry_mass = 1000
 ### EQUATIONS ###
-r = [radius_Earth + initial_orbital_altitude[i] for i in range(len(initial_orbital_altitude))]
-a = [0.5 * (radius_Earth + altitude_for_reentry_dueToDrag + r[i]) for i in range(len(r))]
-orbital_velocity = [np.sqrt((mass_Earth * Grav_constant) / (initial_orbital_altitude[i] + radius_Earth)) for i in range(len(initial_orbital_altitude))]
-V_a = [np.sqrt((mass_Earth * Grav_constant) * ((2 / r[i]) - (1 / a))) for i in range(len(r))]
-# Delta_V = [orbital_velocity - V_a [i] for i in range(len(radius_a1))]
+
+# Delta v computation
+while new_dry_mass - dry_masses[0] > 1:
+    tot_M_prop = np.ones(9, len(dry_masses))
+    for altitude_idx in range(len(initial_orbital_altitude)):
+        r = np.array(radius_Earth + initial_orbital_altitude[altitude_idx]) #km
+        a = np.array(0.5 * (radius_Earth + altitude_for_reentry_dueToDrag + r))
+        orbital_velocity = np.array(np.sqrt((mass_Earth * Grav_constant)/r/1000))
+        V_a = np.array(np.sqrt((mass_Earth * Grav_constant) * ((2 / r/1000) - (1 / a/1000))))
+        Delta_V = np.array(orbital_velocity - V_a)
+        print(Delta_V)
+
+        # From delta v to propellant mass
+        Isp = np.array([283, 117, 210])
+        g0 = 9.80665
+        M_props = np.ones((3, len(dry_masses)))
+        for idx in range(len(M_props[0, :])):
+            M_props[:, idx] = dry_masses[idx]*np.exp(Delta_V/Isp/g0) - dry_masses[idx]
+        print(M_props)
+
+
+        plt.figure(altitude_idx)
+        plt.plot(dry_masses, M_props[0, :], dry_masses, M_props[1, :], dry_masses, M_props[2, :])
+        plt.legend(('Hydrazine', 'Hidrogen Peroxide', 'Propyl Nitrate'))
+        plt.xlabel('Dry mass')
+        plt.ylabel('Propellant mass required for reentry')
+
+        Delta_volumes = np.ones((3, len(M_props[0, :])))
+        for idx in range(len(Prop_density)):
+            Delta_volumes[idx, :] = M_props[idx, :]/Prop_density[idx]
+
+        Delta_dry_mass = Delta_volumes*Tank_density
+        dry_masses += Delta_dry_mass
+        print(Delta_dry_mass)
+plt.show()
+
+
+
+
