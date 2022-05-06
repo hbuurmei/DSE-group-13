87b8618f6d3f75ead825e090af93ae2295897b01
Hugo Buurmeijer <hugo@reef.support>
2022-05-06 11:09:03 +0200

    Add concentration assumption

--- pinch_flux.py
+++ pinch_flux.py
@@ -11,6 +11,6 @@ v_scatter = np.sin(i_scatter)*(r_earth+altitude)
 area = h_scatter*v_scatter
 frequency = n_particles/period
 print('Frequency: ' + str(frequency) + ' debris fragments per second')
-flux = frequency/(area*10**6)*10
+flux = frequency/(area*10**6)*10  # Assume factor of 10 to account for concentration of debris around the original orbit
 print('The flux at the pinch points would be of: ' + str(flux) + ' debris fragments per square meter')
 
