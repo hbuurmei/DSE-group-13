781fa07ed20872ca45c592a88e41ac3caaa93aba
Hugo Buurmeijer <hugo@reef.support>
2022-05-19 16:53:32 +0200

    Include range of inputs.

--- mass_to_slow_down.py
+++ mass_to_slow_down.py
@@ -17,8 +17,23 @@ def slowed_orbit(delV, h, m_dry):
     m = m_dry*np.exp((mu/r**2-V_new**2/2)/(I_sp*g_0)*(t-t_b))
     m_0 = m_dry*np.exp(t_b*(V_new**2/r-mu/r**2)/(I_sp*g_0))  # Initial mass
     m_p = m_0 - m_dry  # Propellant mass
-    return m, t, m_p, t_b
+    return m_p, t_b
 
 
-m, t, m_p, t_b = slowed_orbit(100, 1000e3, 500)
-print(m_p, t_b)
+delV_range = range(10, 1000+10, 10)
+h_range = range(350, 1000+50, 50)
+m_dry_range = range(100, 10000+100, 100)
+
+m_p_min, t_b_min = slowed_orbit(100, 1000e3, 500)
+print(m_p_min, t_b_min)
+
+for delV in delV_range:
+    for h in h_range:
+        for m_dry in m_dry_range:
+            m_p, t_b = slowed_orbit(delV, h, m_dry)
+            if m_p > m_p_min:
+                m_p_min = m_p
+            if t_b < t_b_min:
+                t_b_min = t_b
+
+print(m_p_min, t_b_min)
