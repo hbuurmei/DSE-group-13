63b598e3b12ffc783bda9c0358816190294a68d4
drehling <D.B.Rehling@student.tudelft.nl>
2022-06-01 18:32:54 +0200

    Added the relation between pulse energy and frequency, given a power output.

--- Payload/Cm_Calculator.py
+++ Payload/Cm_Calculator.py
@@ -3,13 +3,14 @@ import numpy as np
 ### Constants ###
 Cm0 = 420E-6
 Lambda = 532E-9
-Pulse_Energy = 80
+E_p = 80
 T_eff = 0.9
 M_squared = 2
 a = 1.7
 z = 250000
-d_s = 0.10
+d_s = 0.11
 tau = 8E-9
+delta_V_needed = 243
 
 
 
@@ -17,10 +18,20 @@ tau = 8E-9
 Fluence_opt = 8.5E8 * np.sqrt(tau)
 Cm = Cm0 / (8.58E8 * Lambda)**(1/4)
 D_eff = a*M_squared*Lambda*z / (d_s)
-Fluence = 4*Pulse_Energy*(D_eff**2)*T_eff / (np.pi * (M_squared**2)*(a**2)*(Lambda**2)*(z**2))
+Fluence_at_debris = 4*E_p*(D_eff**2)*T_eff / (np.pi * (M_squared**2)*(a**2)*(Lambda**2)*(z**2))
 
-print(Fluence_opt)
-print(Cm)
+# print('The optimum fluence you would like is', Fluence_opt)
 # print('The coupling coefficient is', Cm, 'N/W')
-print('The aperture diameter at the space craft is', D_eff, 'm')
-print('The fluence at the target it', Fluence, 'J/m^2')
+# print('The aperture diameter at the space craft is', D_eff, 'm')
+# print('The fluence at the target it', Fluence_at_debris, 'J/m^2')
+
+# Finding the optimum E and f
+Power_limit = 29364.13333
+Usable_Power = Power_limit*np.cos(np.pi/180 * 20)**(4/3)*0.37
+print(Usable_Power)
+f = Usable_Power / E_p
+AMR = 0.08
+a_deb = Fluence_at_debris * AMR * Cm * f
+time_to_ablate = delta_V_needed/a_deb
+print(time_to_ablate)
+
