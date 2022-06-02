ff66c42885e91392b10e0be563e14e458368c24b
drehling <D.B.Rehling@student.tudelft.nl>
2022-06-02 08:51:29 +0200

    Changed Energy to be the output

--- Payload/Cm_Calculator.py
+++ Payload/Cm_Calculator.py
@@ -3,7 +3,7 @@ import numpy as np
 ### Constants ###
 C_m0 = 420E-6
 Lambda = 532E-9
-E_p = 80
+E_p = 90
 T_eff = 0.9
 M_squared = 2
 a = 1.7
@@ -11,7 +11,7 @@ a = 1.7
 ### Desired values ###
 z = 250000
 d_s = 0.11
-tau = 8E-9
+tau = 1E-10
 delta_V_needed = 243
 
 # Dependent variables ###
@@ -19,6 +19,9 @@ fluence_opt = 8.5E8 * np.sqrt(tau)
 C_m = C_m0 / (8.58E8 * Lambda)**(1/4)
 D_eff = a*M_squared*Lambda*z / (d_s)
 fluence_at_debris = 4*E_p*(D_eff**2)*T_eff / (np.pi * (M_squared**2)*(a**2)*(Lambda**2)*(z**2))
+
+# E = fluence_opt / (4*(D_eff**2)*T_eff / (np.pi * (M_squared**2)*(a**2)*(Lambda**2)*(z**2)))
+# print(E)
 
 print('The optimum fluence is', fluence_opt)
 #print('The coupling coefficient is', Cm, 'N/W')
@@ -28,11 +31,10 @@ print('The fluence at the target it', fluence_at_debris, 'J/m^2')
 ### Finding the optimum E and f ###
 
 ### Constants ###
-power_limit = 29364.13333
+power_limit = 20000
 efficiency_conversion = 0.37
 maximum_ablation_angle = 20
 incidence_angle_efficiency = np.cos(np.pi/180 * maximum_ablation_angle)**(4/3)
-
 
 usable_power = power_limit * efficiency_conversion * incidence_angle_efficiency
 
@@ -41,5 +43,7 @@ f = usable_power / E_p
 AMR = 0.08
 a_deb = fluence_at_debris * AMR * C_m * f
 tt_dv = delta_V_needed/a_deb
+
+print('The frequency of the laser is', f, 'Hz')
 print('The time to impart dV is', tt_dv, 's')
 
