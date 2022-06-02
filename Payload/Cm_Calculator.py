d3564bbee69b0d2c75916c8fe73c11ad12d45b31
drehling <D.B.Rehling@student.tudelft.nl>
2022-06-02 10:29:09 +0200

    Changed Energy to be the output

--- Payload/Cm_Calculator.py
+++ Payload/Cm_Calculator.py
@@ -10,23 +10,23 @@ a = 1.7
 
 ### Desired values ###
 z = 250000
-d_s = 0.11
+d_s = 0.13
 tau = 1E-10
-delta_V_needed = 243
+delta_V_needed = 214.096274016425
 
 # Dependent variables ###
 fluence_opt = 8.5E8 * np.sqrt(tau)
-C_m = C_m0 / (8.58E8 * Lambda)**(1/4)
+C_m = C_m0 / (8.5E8 * Lambda)**(1/4)
 D_eff = a*M_squared*Lambda*z / (d_s)
-fluence_at_debris = 4*E_p*(D_eff**2)*T_eff / (np.pi * (M_squared**2)*(a**2)*(Lambda**2)*(z**2))
+# fluence_at_debris = 4*E_p*(D_eff**2)*T_eff / (np.pi * (M_squared**2)*(a**2)*(Lambda**2)*(z**2))
 
-# E = fluence_opt / (4*(D_eff**2)*T_eff / (np.pi * (M_squared**2)*(a**2)*(Lambda**2)*(z**2)))
-# print(E)
+E = fluence_opt / (4*(D_eff**2)*T_eff / (np.pi * (M_squared**2)*(a**2)*(Lambda**2)*(z**2)))
+print(E)
 
 print('The optimum fluence is', fluence_opt)
 #print('The coupling coefficient is', Cm, 'N/W')
 print('D_eff is', D_eff, 'm')
-print('The fluence at the target it', fluence_at_debris, 'J/m^2')
+
 
 ### Finding the optimum E and f ###
 
@@ -40,10 +40,11 @@ usable_power = power_limit * efficiency_conversion * incidence_angle_efficiency
 
 
 f = usable_power / E_p
-AMR = 0.08
-a_deb = fluence_at_debris * AMR * C_m * f
+AMR = 0.076
+a_deb = fluence_opt* AMR * C_m * f
 tt_dv = delta_V_needed/a_deb
 
 print('The frequency of the laser is', f, 'Hz')
 print('The time to impart dV is', tt_dv, 's')
+print('The energy in kWh is', power_limit*tt_dv/3600000)
 
